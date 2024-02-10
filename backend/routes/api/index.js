b7618e97cd2a1f380890166b52233c3db5e26c83
John McElroy <mcelrj@gmail.com>
2024-02-10 16:45:09 -0600

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
@@ -7,7 +9,15 @@ const { restoreUser } = require("../../utils/auth.js");
 router.use(restoreUser);
 
 
+router.use('/session', sessionRouter);
 
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
+module.exports = router;
 
 //original verification testing follows. Ok to remove.
 
@@ -50,8 +60,3 @@ router.use(restoreUser);
 //   setTokenCookie(res, user);
 //   return res.json({ user: user });
 // });
-
-
-
-
-module.exports = router;
