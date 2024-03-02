7ee3a35e83d790a8f6e7f79c18024aca8b100983
John McElroy <mcelrj@gmail.com>
2024-03-02 15:20:12 -0600

    delete review image finished

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -8,11 +8,50 @@ const { requireAuth } = require('../../utils/auth');
 // const { check } = require('express-validator');
 // const { handleValidationErrors } = require('../../utils/validation');
 const { stub } = require('../../utils/utils');
+const { ReviewImage } = require('../../db/models');
 
 
 //REQ AUTH - Delete a Review Image
-router.delete('/:imageId', requireAuth, stub,
-    (req, res) => {}
+router.delete('/:imageId', requireAuth,
+  async (req, res) => {
+    const {imageId} = req.body
+
+    if (imageId === undefined){
+        const err = new Error("Review Image couldn't be found");
+        err.title = "Resource Not Found";
+        err.status = 404;
+        throw err;
+    }
+
+    let imgs = await ReviewImage.findAll(
+      {
+          where: {id: imageId},
+          include: ['Review']
+      }
+    );
+
+    if (imgs.length === 0){
+      res.statusCode = 404
+      return res.json({
+        "message": "Review  Image couldn't be found"
+      })
+    }
+    const img = imgs[0]
+
+    if (img.dataValues.Review.dataValues.userId != req.user.id){
+      res.statusCode = 401
+      return res.json({
+        "message": "User not authorized."
+      })
+    }
+
+    await img.destroy();
+
+    return res.json(
+      {
+        message: "Successfully deleted"
+    });
+  }
 );
 
 module.exports = router;
