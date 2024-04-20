cb2dd9bfcc9bc41ed84d8d519f3b881e9a5feb9d
John McElroy <mcelrj@gmail.com>
2024-04-20 15:42:34 -0500

    basic spot detail complete

--- frontend/src/components/ReviewItem/index.js
+++ frontend/src/components/ReviewItem/index.js
@@ -1,9 +1,27 @@
 import './ReviewItem.css'
 
 function ReviewItem({ review }) {
-console.log('rrr', review)
+  console.log('EL review', review)
+
+  function getDate(){
+    const months = ['January', 'February', 'March',
+     'April', 'May', 'June', 'July', 'August',
+     'September', 'October', 'November', 'December'];
+
+    const date = new Date(review.updatedAt)
+
+    return `${months[date.getMonth()]} ${date.getFullYear()}`;
+
+  }
+
   return (
       <>
+      <div className='redBox item'>
+
+        <p>{review.User.firstName}</p>
+        <p>{getDate()}</p>
+        <p>{review.review}</p>
+      </div>
       </>
     );
   }
